117430658e9eade39cedb3ea5f287f431e3019bd
Quinlan Duval <quinlan.duval@comcast.net>
2022-02-23 02:16:12 -0500

    No longer crashes when a resource already exists

--- mc-deploy.py
+++ mc-deploy.py
@@ -1,48 +1,60 @@
+import io
 import yaml
 from os import path
-import io
 from kubernetes import client, config
 
 def create_pv(file: io.TextIOWrapper):
     v1 = client.CoreV1Api()
     vol = yaml.safe_load(file)
-    resp = v1.create_persistent_volume(vol)
-    print(f"Persistent volume created. status={resp.metadata.name}")
+    try:
+        resp = v1.create_persistent_volume(vol)
+    except client.rest.ApiException as e:
+        print(f"Error creating PersistentVolume:\n{e}")
+        return
+    print(f"PersistentVolume {resp.metadata.name} created.")
     
-
-def create_volume_claim(file: io.TextIOWrapper):
+def create_volume_claim(file: io.TextIOWrapper, namespace="default"):
     v1 = client.CoreV1Api()
     pvc = yaml.safe_load(file)
-    resp = v1.create_namespaced_persistent_volume_claim("default", pvc)
-    print(f"Persistent volume claim created. status={resp.metadata.name}")
+    try:
+        resp = v1.create_namespaced_persistent_volume_claim(namespace, pvc)
+    except client.rest.ApiException as e:
+        print(f"Error creating PersistentVolumeClaim:\n{e}")
+        return
+    print(f"PersistentVolumeClaim created. status={resp.metadata.name}")
 
-def create_deployment(file: io.TextIOWrapper):
+def create_deployment(file: io.TextIOWrapper, namespace="default"):
     v1 = client.AppsV1Api()
     dep = yaml.safe_load(file)
-    resp = v1.create_namespaced_deployment("default", dep)
+    try:
+        resp = v1.create_namespaced_deployment(namespace, dep)
+    except client.rest.ApiException as e:
+        print(f"Error creating Deployment:\n{e}")
+        return
     print(f"Deployment created. status={resp.metadata.name}")
 
-def create_nodeport(file: io.TextIOWrapper):
+def create_nodeport(file: io.TextIOWrapper, namespace="default"):
     v1 = client.CoreV1Api()
-    np = yaml.safe_load(file)
-    resp = v1.create_namespaced_service("default", np)
+    NodePort = yaml.safe_load(file)
+    try:
+        resp = v1.create_namespaced_service(namespace, NodePort)
+    except client.rest.ApiException as e:
+        print(f"Error creating NodePort:\n{e}")
+        return
     print(f"NodePort created. status={resp.metadata.name}")
 
 def main():
     config.load_kube_config()
     manifestDir = path.join(path.dirname(__file__), "manifests/minecraft/")
 
-    # with open(path.join(manifestDir, "volume.yaml")) as file:
-    #     create_pv(file)
-    # with open(path.join(manifestDir, "volume-claim.yaml")) as file:
-    #     create_volume_claim(file)
+    with open(path.join(manifestDir, "volume.yaml")) as file:
+        create_pv(file)
+    with open(path.join(manifestDir, "volume-claim.yaml")) as file:
+        create_volume_claim(file)
     with open(path.join(manifestDir, "deployment.yaml")) as file:
         create_deployment(file)
-    # with open(path.join(manifestDir, "nodeport.yaml")) as file:
-    #     create_nodeport(file)
-
-
-
+    with open(path.join(manifestDir, "nodeport.yaml")) as file:
+        create_nodeport(file)
 
 if __name__ == '__main__':
     main()
