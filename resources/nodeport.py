e0ec81e39246073e29565af652b229e109921205
Quinlan Duval <quinlan.duval@comcast.net>
2022-02-24 04:05:23 -0500

    added nodeport management functions

--- resources/nodeport.py
+++ resources/nodeport.py
@@ -1,14 +1,40 @@
 from kubernetes import client, config
 
-def create_nodeport(volume):
-    pass
+def create_nodeport(port: int, selector: str, namespace="default"):
+    api = client.CoreV1Api()
 
-def delete_nodeport(nodeport):
-    pass
+    spec = client.V1ServiceSpec(
+        selector = {"app": selector},
+        ports = [
+            client.V1ServicePort(
+                port=port
+            )
+        ],
+        type="NodePort"
+    )
+
+    body = client.V1Service(
+        api_version = "v1",
+        kind = "Service",
+        metadata = client.V1ObjectMeta(
+            name = selector
+        ),
+        spec = spec
+    )
+
+    resp = api.create_namespaced_service(namespace, body)
+
+def delete_nodeport(name, namespace):
+    api = client.CoreV1Api()
+    resp = api.delete_namespaced_service(name, namespace)
+    
 
 def main():
     config.load_kube_config()
     core_v1 = client.CoreV1Api()
+
+    create_nodeport(25565, "test", "dev")
+    delete_nodeport("test", "dev")
 
 if __name__ == "__main__":
     main()
