952f948e96868273f28e790133377a6a0e3b1245
Quinlan Duval <quinlan.duval@comcast.net>
2022-02-26 16:28:33 -0500

    added print statements for logging, added try/catch to API calls

--- resources/nodeport.py
+++ resources/nodeport.py
@@ -2,38 +2,48 @@ from kubernetes import client, config
 
 def create_nodeport(port: int, selector: str, namespace="default"):
     api = client.CoreV1Api()
+    name = selector
 
     spec = client.V1ServiceSpec(
         selector = {"app": selector},
-        ports = [
-            client.V1ServicePort(
-                port=port
-            )
-        ],
+        ports = [client.V1ServicePort(port=port)],
         type="NodePort"
     )
 
     body = client.V1Service(
         api_version = "v1",
         kind = "Service",
-        metadata = client.V1ObjectMeta(
-            name = selector
-        ),
+        metadata = client.V1ObjectMeta(name = name),
         spec = spec
     )
 
-    resp = api.create_namespaced_service(namespace, body)
+    print(f"Creating NodePort {name} in namespace {namespace}.")
+    try:
+        resp = api.create_namespaced_service(namespace, body)
+    except client.rest.ApiException as e:
+        print(f"NodePort creation failed:\n{e}")
+        return
+    print(f"NodePort {name} successfully created.")
+    return resp
 
 def delete_nodeport(name, namespace):
     api = client.CoreV1Api()
-    resp = api.delete_namespaced_service(name, namespace)
+    
+    print(f"Deleting NodePort {name} from namespace {namespace}.")
+    try:
+        resp = api.delete_namespaced_service(name, namespace)
+    except client.rest.ApiException as e:
+        print(f"NodePort creation failed:\n{e}")
+        return
+    print(f"NodePort {name} successfully deleted.")
+    return resp
     
 
 def main():
     config.load_kube_config()
-    core_v1 = client.CoreV1Api()
 
     create_nodeport(25565, "test", "dev")
+    input("Press Enter to continue: ")
     delete_nodeport("test", "dev")
 
 if __name__ == "__main__":
