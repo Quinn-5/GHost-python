f78901c693fbd16938c4a258a8e79d234ff35bd5
Quinlan Duval <quinlan.duval@comcast.net>
2022-02-26 19:33:54 -0500

    PersistentVolume module now configured to create static volumes

--- resources/volume.py
+++ resources/volume.py
@@ -1,14 +1,66 @@
 from kubernetes import client, config
 
-def create_volume(volume):
-    pass
+def create_volume(size: str, name, namespace="default"):
+    api = client.CoreV1Api()
 
-def delete_volume(volume):
-    pass
+    spec = client.V1PersistentVolumeSpec(
+        access_modes=["ReadWriteOnce"],
+        capacity={"storage":size},
+        persistent_volume_reclaim_policy="Delete",
+        local=client.V1LocalVolumeSource(
+            path=f"/mnt/kube/{namespace}/{name}"
+        ),
+        node_affinity=client.V1VolumeNodeAffinity(
+            required=client.V1NodeSelector(
+                node_selector_terms=[
+                    client.V1NodeSelectorTerm(
+                        match_expressions=[
+                            client.V1NodeSelectorRequirement(
+                                key="kubernetes.io/hostname",
+                                operator="In",
+                                values=["amadeus"]
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
+    )
+
+    body = client.V1PersistentVolume(
+        api_version="v1",
+        kind="PersistentVolume",
+        metadata=client.V1ObjectMeta(name=name),
+        spec=spec
+    )
+
+    print(f"Creating PersistentVolume {name}.")
+    try:
+        resp = api.create_persistent_volume(body)
+    except client.rest.ApiException as e:
+        print(f"PersistentVolume creation failed:\n{e}")
+        return
+    print(f"PersistentVolume {name} successfully created.")
+    return resp
+
+def delete_volume(name: str):
+    api = client.CoreV1Api()
+
+    print(f"Deleting PersistentVolume {name}.")
+    try:
+        resp = api.delete_persistent_volume(name)
+    except client.rest.ApiException as e:
+        print(f"PersistentVolume deletion failed:\n{e}")
+        return
+    print(f"PersistentVolume {name} successfully deleted.")
+    return resp
 
 def main():
     config.load_kube_config()
-    core_v1 = client.CoreV1Api()
+
+    create_volume("500Mi", "test", "dev")
+    input("Press Enter to continue...")
+    delete_volume("test")
 
 if __name__ == "__main__":
     main()
