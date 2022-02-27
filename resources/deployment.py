5a10f11c651ec6b42c55d8d99b1534251eb84619
Quinlan Duval <quinlan.duval@comcast.net>
2022-02-27 03:12:54 -0500

    added deployment creation functions - untested

--- resources/deployment.py
+++ resources/deployment.py
@@ -1,14 +1,35 @@
 from kubernetes import client, config
 
-def launch_deployment(deployment):
-    pass
+def launch_deployment(deployment: client.V1Deployment, namespace="default"):
+    api = client.AppsV1Api()
 
-def delete_deployment(deployment):
-    pass
+    print(f"Creating Deployment {name}.")
+    try:
+        resp = api.create_namespaced_deployment(namespace, deployment)
+    except client.rest.ApiException as e:
+        print(f"Deployment creation failed:\n{e}")
+        return
+    print(f"Deployment {name} successfully created.")
+    return resp
+
+def delete_deployment(name: str, namespace="default"):
+    api = client.AppsV1Api()
+
+    print(f"Deleting Deployment {name}.")
+    try:
+        resp = api.delete_namespaced_deployment(name, namespace)
+    except client.rest.ApiException as e:
+        print(f"Deployment deletion failed:\n{e}")
+        return
+    print(f"Deployment {name} successfully deleted.")
+    return resp
 
 def main():
     config.load_kube_config()
-    apps_v1 = client.AppsV1Api()
+
+    launch_deployment(deployment)
+    input("Press Enter to continue: ")
+    delete_deployment(name)
 
 if __name__ == "__main__":
     main()
